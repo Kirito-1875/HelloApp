90d35356b094eed3f2585dd3d67eef699d267145
Kirito-1875 <pm9920@srmist.edu.in>
2026-03-18 14:28:35 +0530

    [200~Display Hello with Multiple Command-Line Arguments or Default Message
    ;

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,6 @@
 public class HelloApp{
 	public static void main(String[] args){
-		String name = "World";
-		if (args.length > 0) {
-			name = args[0];
-		}
+		name = args;
 		System.out.println("Hello, " + name + "!");
 	}
 }
