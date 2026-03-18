0337b9aec69f1c8e4989ba101348b18b66af9992
Kirito-1875 <pm9920@srmist.edu.in>
2026-03-18 14:56:03 +0530

    final

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,9 @@
 public class HelloApp{
 	public static void main(String[] args){
-		name = args;
+		String name = "World";
+		if (args.length > 0) {
+			name = args.join(", ");
+		}
 		System.out.println("Hello, " + name + "!");
 	}
 }
