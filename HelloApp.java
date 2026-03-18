279b7f553a996a6dfd8de8fb2406eafb23eb870c
Kirito-1875 <pm9920@srmist.edu.in>
2026-03-18 15:10:07 +0530

    Enhanced For Loop

--- HelloApp.java
+++ HelloApp.java
@@ -2,8 +2,13 @@ public class HelloApp{
 	public static void main(String[] args){
 		String name = "World";
 		if (args.length > 0) {
-			name = String.join(", ", args);
+			System.out.print("Hello, ");
+			for (int i = 0; i < args.length ; i++) {
+				System.out.print(args[i] + ", ");
+			}
+			System.out.print("!");
+		} else {
+			System.out.println("Hello, " + name + "!");
 		}
-		System.out.println("Hello, " + name + "!");
 	}
 }
